f68593ddc22f3223d8f7eab785dac969b0a31566
Umrao Bisht <umraobisht1@gmail.com>
2024-08-07 14:57:54 +0530

    fix minor changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy: {
-      '/api': "https://employees-crud-melj.onrender.com/"
-    }
+      '/api': {
+        target: "https://employees-crud-melj.onrender.com/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   }
 })
